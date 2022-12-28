360970b87999e2555916c756954a5748c79f7b24
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2022-12-28 17:41:23 +0200

    vomits and dies

--- lua/cgh/sh_grapplinghook.lua
+++ lua/cgh/sh_grapplinghook.lua
@@ -16,8 +16,9 @@ if CLIENT then
 end
 
 hook.Add("SetupMove", "chensgrapplinghook", function(ply, mv, cmd)
+    -- print(mv:GetButtons())
     if !ply:IsValid() or !IsFirstTimePredicted() then return end
-    if mv:KeyPressed(IN_ATTACK3) && !ply:InVehicle() then
+    if mv:KeyPressed(33554432) && !ply:InVehicle() then
         trace = util.TraceLine({
             start = ply:EyePos(),
             endpos = ply:EyePos() + ply:GetAimVector() * dist,
@@ -31,13 +32,13 @@ hook.Add("SetupMove", "chensgrapplinghook", function(ply, mv, cmd)
         ply:EmitSound("NPC_Combine.Zipline_MidClothing")
     end
     if trace.Hit && ply:GetMoveType() == MOVETYPE_WALK then
-        if mv:KeyDown(IN_ATTACK3) && CurTime() > ct then
+        if mv:KeyDown(33554432) && CurTime() > ct then
             vel = (dir - ply:EyePos()) * power / dist * 0.5 / trace.Fraction
             ResetCurTime()
             mv:SetVelocity(mv:GetVelocity() + vel)
             debugoverlay.Line(dir, ply:EyePos(), 0.1)
             -- print(vel, mv:GetVelocity():Length())
-        elseif mv:KeyReleased(IN_ATTACK3) then
+        elseif mv:KeyReleased(33554432) then
             ply.grappling = false
             ply:EmitSound("d1_town.CarRelease")
         end
